251eddaef2d9e025236d47531c671f3982a592c9
leandrocodigo5 <leandro@codigo5.com.br>
2025-08-25 08:50:54 -0400

    deploy com o restante

---
+++ web/wp-config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('AS3CF_SETTINGS', serialize(array(
+    'provider' => 'do',
+    'access-key-id' => 'LZQAHCBDGFOLQR5UUHFR',
+    'secret-access-key' => 'QVrgE+F/Rr0IDkZF5y0AdPtnnh2VMuPo8cCVrdxKzX4',
+)));
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/config/application.php';
+require_once ABSPATH . 'wp-settings.php';
